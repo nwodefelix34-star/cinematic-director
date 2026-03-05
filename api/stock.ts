9684227f1a7214fe37c5bb55e31e72949def9701
nwodefelix34-star <nwodefelix34@gmail.com>
2026-03-05 05:57:07 +0100

    Change image size from small to regular

--- api/stock.ts
+++ api/stock.ts
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
     const unsplashData = await unsplash.json();
 
     return res.status(200).json({
-      images: unsplashData.results.map(img => img.urls.small)
+      images: unsplashData.results.map(img => img.urls.regular)
     });
 
   } catch (error) {
